bd5636cbc6e4ae1d3a6a4fd3e247156e3c7dd269
Rishi Vishwakarma <rishi0@outlook.in>
2025-01-27 13:54:41 +0530

    websocket (wip)

--- src/index.js
+++ src/index.js
@@ -1,27 +1,67 @@
-require('dotenv').config()
+import { config } from "dotenv";
+import { MongoClient, ServerApiVersion } from "mongodb";
+import { WebSocketServer } from "ws";
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+config();
 const uri = `mongodb+srv://r:${process.env.dbpassforr}@cluster.eomop.mongodb.net/?retryWrites=true&w=majority&appName=cluster`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        // strict: true,
+        deprecationErrors: true,
+    },
 });
 
-async function run() {
-  try {
-    await client.connect();
-    const database = client.db('sample_mflix');
-    const movies = database.collection('movies');
-    const query = { title: 'Back to the Future' };
-    const movie = await movies.findOne(query);
-    console.log(movie);
-  } finally {
-    await client.close();
-  }
-}
-run().catch(console.dir);
+const wss = new WebSocketServer({ port: 8000 });
+
+const agg = [
+    {
+        $search: {
+            text: {
+                query: "Summer",
+                path: "title",
+            },
+        },
+    },
+    {
+        $limit: 5,
+    },
+
+    {
+        $project: {
+            _id: 0,
+            title: 1,
+        },
+    },
+];
+
+client
+    .connect({ useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        const database = client.db("sample_mflix");
+        const movies = database.collection("movies");
+
+        wss.on("connection", (ws, req) => {
+            // handle auth
+            console.log("recv conn");
+
+            ws.on("message", async (msg) => {
+                const data = JSON.parse(msg.toString());
+                if (data.t == "autoc") {
+                    // execute search
+                    console.log("searching");
+
+                    let cursor = await movies.aggregate(agg);
+                    await cursor.forEach((doc) => console.log(doc));
+                    ws.send(JSON.stringify({ t: "autocreply", data: "hello" }));
+                }
+            });
+        });
+    })
+    .catch(console.dir);
+// .finally(() => {
+//     client.close().then(() => {
+//         console.log("connection to db closed");
+//     });
+// });
