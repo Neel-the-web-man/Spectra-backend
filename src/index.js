64ba05707895cc2d01d31c5f1c81c6c6a3f7e5f4
Rishi Vishwakarma <rishi0@outlook.in>
2025-01-31 02:47:34 +0530

    working with 2 db

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import { app } from "./app.js";
 import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
 import { WebSocketServer } from "ws";
 
+import mongoose from "mongoose";
+
+import { Movie } from "./models/movie.model.js";
+
 connectDB()
     .then(() => {
         app.listen(process.env.PORT || 8000, () => {
@@ -14,6 +18,61 @@ connectDB()
     .catch((err) => console.log("neel db error", err));
 
 const wss = new WebSocketServer({ port: 8001 });
+
+
+
+
+
+
+
+
+
+
+
+    // const client = mongoose.connection.getClient();
+    // const database = client.db("opensoft_db");
+    // const movies = database.collection("movies");
+
+
+const autocompleteMovies = async (searchTerm) => {
+
+    const pipeline = [
+        {
+            $search: {
+                autocomplete: {
+                    query: searchTerm,
+                    path: "title",
+                    fuzzy: {
+                        maxEdits: 2,
+                    },
+                },
+            },
+        },
+        {
+            $limit: 10,
+        },
+        {
+            $project: {
+                title: 1,
+                poster: 1,
+            },
+        },
+    ];
+
+    const results = await movies.aggregate(pipeline).toArray();
+    return results;
+};
+
+
+
+
+
+
+
+
+
+
+
 
 const uri =
     `mongodb+srv://r:${process.env.dbpassforr}@cluster.eomop.mongodb.net/?retryWrites=true&w=majority&appName=cluster`;
@@ -32,17 +91,23 @@ client
         const database = client.db("opensoft_db");
         const movies = database.collection("movies");
 
+        
+        
+
         console.log("started");
 
         wss.on("connection", (ws) => {
             // handle auth
             console.log("recv conn");
 
-            ws.on("message", async (msg) => {
+            ws.on("message", async (msg) => {   
+                // console.log(await movies.findOne());
                 const data = JSON.parse(msg.toString());
                 if (data.t == "autoc" && data.data) {
                     // execute search
                     console.log("searching");
+
+                    // const result = await autocompleteMovies(data.data);
                     const result = await movies.aggregate([
                         {
                             $search: {
@@ -67,6 +132,9 @@ client
                     );
                 } else if (data.t == "info" && data.data) {
                     console.log("recv for info");
+
+                    // const id = new mongoose.Types.ObjectId(data.data);
+                    // const document = await Movie.findById(id);
 
                     const id = new ObjectId(data.data);
                     const document = await movies.findOne({
