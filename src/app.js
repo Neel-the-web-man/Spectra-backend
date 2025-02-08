6e290956aef17c5e4a871e472da5a7e399455ddd
Rishi Vishwakarma <rishi0@outlook.in>
2025-02-08 23:50:01 +0530

    index route

--- src/app.js
+++ src/app.js
@@ -4,21 +4,21 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  }),
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
 );
 app.use(
-  express.json({
-    limit: "16kb",
-  }),
+    express.json({
+        limit: "16kb",
+    })
 );
 app.use(
-  express.urlencoded({
-    extended: true,
-    limit: "16kb",
-  }),
+    express.urlencoded({
+        extended: true,
+        limit: "16kb",
+    })
 );
 app.use(express.static("public"));
 app.use(cookieParser());
@@ -27,5 +27,9 @@ import userRouter from "./routes/user.routes.js";
 
 //routes declaration
 app.use("/users", userRouter);
+
+app.get('/', () => {
+    return 'Hello, running'
+})
 
 export { app };
